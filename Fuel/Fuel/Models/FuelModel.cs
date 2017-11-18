8cb46a9f42a3ba00e5f2d160f29712c898f98114
LOKI <jakub.tor@o2.pl>
2017-11-18 09:49:24 +0100

    zmiany

---
+++ Fuel/Fuel/Models/FuelModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fuel.Models
+{
+    public class FuelModel
+    {
+        public int Fuel { get; set; }
+        public int Distance { get; set; }
+        public int AgComsumption{ get; set; }
+    }
+}
---
+++ Fuel/Fuel/ViewModel/FuelviewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fuel.ViewModel
+{
+    public class FuelviewModel
+    {
+        public FuelviewModel Fuelmodel { get; set; }
+        public bool ShowResult { get; set; }
+    }
+}
